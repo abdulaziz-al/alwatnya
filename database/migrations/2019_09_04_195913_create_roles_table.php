b443edac835067d4a83f303be12b3205c9ed1681
faisal-sameer <43131907+faisal-sameer@users.noreply.github.com>
2019-09-04 23:32:14 +0300

    roleTable

--- database/migrations/2019_09_04_195913_create_roles_table.php
+++ database/migrations/2019_09_04_195913_create_roles_table.php
@@ -14,8 +14,9 @@ class CreateRolesTable extends Migration
     public function up()
     {
         Schema::create('roles', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
+            $table->increments('role_id');
+            $table->string('role_name');
+              $table->timestamps();
             
         });
     }
