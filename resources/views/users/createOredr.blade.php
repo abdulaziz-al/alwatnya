c352d444da50ab4ce1fe0a1659e438658b924db3
abdulaziz-al <abdulaziz_AF@github.com>
2019-09-17 02:03:43 +0300

    save

--- resources/views/users/createOredr.blade.php
+++ resources/views/users/createOredr.blade.php
@@ -33,7 +33,72 @@
 
 
 
-
+ <script>
+    $(document).ready(function(){
+    
+     var count = 1;
+    
+     dynamic_field(count);
+    
+     function dynamic_field(number)
+     {
+      html = '<tr>';
+            html += '<td><input type="text" name="first_name[]" class="form-control" /></td>';
+            html += '<td><input type="file" name="last_name[]" class="form-control" /></td>';
+            if(number > 1)
+            {
+                html += '<td><button type="button" name="remove" id="" class="btn btn-danger remove">Remove</button></td></tr>';
+                $('tbody').append(html);
+            }
+            else
+            {   
+                html += '<td><button type="button" name="add" id="add" class="btn btn-success">Add</button></td></tr>';
+                $('tbody').html(html);
+            }
+     }
+    
+     $(document).on('click', '#add', function(){
+      count++;
+      dynamic_field(count);
+     });
+    
+     $(document).on('click', '.remove', function(){
+      count--;
+      $(this).closest("tr").remove();
+     });
+    
+     $('#dynamic_form').on('submit', function(event){
+            event.preventDefault();
+            $.ajax({
+                method:'post',
+                data:$(this).serialize(),
+                dataType:'json',
+                beforeSend:function(){
+                    $('#save').attr('disabled','disabled');
+                },
+                success:function(data)
+                {
+                    if(data.error)
+                    {
+                        var error_html = '';
+                        for(var count = 0; count < data.error.length; count++)
+                        {
+                            error_html += '<p>'+data.error[count]+'</p>';
+                        }
+                        $('#result').html('<div class="alert alert-danger">'+error_html+'</div>');
+                    }
+                    else
+                    {
+                        dynamic_field(1);
+                        $('#result').html('<div class="alert alert-success">'+data.success+'</div>');
+                    }
+                    $('#save').attr('disabled', false);
+                }
+            })
+     });
+    
+    });
+    </script>
 
 
 
