c464adb79902e58ab568e11028073adbae9e2e1f
abdulaziz-al <abdulaziz_AF@github.com>
2019-09-17 01:19:18 +0300

    navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,50 +29,108 @@
         </style>
     </head>
     <body>
-         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">
-                
-                
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+         <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
             <img  src="images/Logo-white-Transparent-Background.png" width="60" height="60" class="d-inline-block align-top" alt="">
-            
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ ('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+         
             </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <ul class="navbar-nav ml-auto">
+          
+
+
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                     <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
                         <a class="nav-link" href="/contact-us">تواصل معنا</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link"  href="/checkstatus">استعلام عن طلب</a>
                     </li>
-                    <li class="nav-item active">
-                        
-                           
-                        <a class="nav-link" href="/login">الدخول  <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/user">التسجيل</a>
-                    </li>
-                    <li class="nav-item">
+
+                     <li class="nav-item">
                         <a class="nav-link" href="/">الرئيسية</a>
                     </li>
-                    <li>
-                            <li>  <a href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                              document.getElementById('logout-form').submit();">
-                                 {{ __('تسجيل حروج') }}
-                             </a></li>
-
-                             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                 @csrf
-                             </form>
-                        
+    
+                            @guest
+           
+              <li class="nav-item">
+                           <a class="nav-link" href="/login">الدخول  <span class="sr-only">(current)</span></a>
                     </li>
-                </ul>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/register">التسجيل</a>
+                    </li>
+                        @else
+         
+                    @if (Auth::user()->role_id == 3 )
+                    
+                    <li class="nav-item">
+                            <a class="nav-link" href="/user">لوحة التحكم</a>
+                        </li>
+
+                    <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                الوصل السريع  <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#"
+                                    ><!--add count for each option-->
+                                    {{ __('إنشاء طلب ') }}
+                                </a>
+                                        <a class="dropdown-item" href="#"
+                                           >
+                                            {{ __('الطلبات الحالية ') }}
+                                        </a>
+                                                <a class="dropdown-item" href="#"
+                                                   >
+                                                    {{ __('الطلبات المنفذة') }}
+                                                </a>
+                                                        <a class="dropdown-item" href="#"
+                                                         >
+                                                            {{ __('الطلبات المعادة') }}
+                                                        </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                        
+                    @endif
+
+                      
+                    
+                   
+                      <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->full_name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
             </div>
         </nav>
+    
+                         
+                          
 
         @yield('content')
                 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
