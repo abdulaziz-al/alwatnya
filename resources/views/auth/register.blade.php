68c519d880bd99db0133c12d4abe3cff94641a7d
faisal-sameer <43131907+faisal-sameer@users.noreply.github.com>
2019-10-06 16:17:35 +0300

    edit_field_task3

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
         <div class="col-md-3 register-left">
             <img id="image_reg" src="images/Logo-Transparent-Background.png" alt=""/>
 
-            <h3>أهلا وسهلا</h3>
+            <h4>أهلا وسهلا</h4>
             <p> شركة الوطنية للتخليص الجمركي</p>
             <a href="/login" > 
             <input type="submit" name=""  value="تسجيل دخول"/>
@@ -21,51 +21,50 @@
             
             <div class="tab-content" id="myTabContent">
                 <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                    <h3 class="register-heading">سجل</h3>
+                    <h1 class="register-heading">سجل</h1>
                     <form method="POST" action="{{ route('register') }}" >
 
                         @csrf
     
                     <div class="row register-form">
-                        <div class="col-md-6">
-                        
+                            <div class="col-lg-12">
+
+
+                                        <i class="fa fa-user" aria-hidden="true" id="icon" ></i>
+                             <input id="typesPrompt" name="full_name" type="text"  class="form-control" placeholder="الإسم الكامل" id="typesPrompt" >
+                             <span id="typePrompt">يرجى إدخال إسم المستخدم الكامل</span>
+    
+                            
+                              
+
+
+
                              
-                            <div class="form-group row">
+                            
                                     <i class="fa fa-envelope-open" aria-hidden="true" id="icon"></i>
                                 <input id="typesPrompt"  name="email" type="text" class="form-control" placeholder="إيميل " value="" />
                                 <span id="typePrompt">يرجى إدخال الإيميل</span>
 
-                            </div>
-                            <div class="form-group">
-                                    <i class="fa fa-phone" aria-hidden="true" id="icon"></i>
-                                <input  id="typesPrompt" type="text" minlength="10" maxlength="10" name="phone" class="form-control" placeholder="رقم جوال*" value="" />
-                                <span id="typePrompt">يرجى إدخال الجوال</span>
-
-                            </div>
-
+                            
+                            
 
                            
 
                            
 
 
-                        </div>
-                        <div class="col-md-6">
-                           
+                        
 
 
-                            <div class="form-group row">
-                                    <i class="fa fa-user" aria-hidden="true" id="icon" ></i>
-                         <input id="typesPrompt" name="full_name" type="text"  class="form-control" placeholder="الإسم الكامل" id="typesPrompt" >
-                         <span id="typePrompt">يرجى إدخال إسم المستخدم الكامل</span>
-
-                        </div>
-                          
+                                        <i class="fa fa-phone" aria-hidden="true" id="icon"></i>
+                                    <input  id="typesPrompt" type="text" minlength="10" maxlength="10" name="phone" class="form-control" placeholder="رقم جوال*" value="" />
+                                    <span id="typePrompt">يرجى إدخال الجوال</span>
+    
+    
                             
                             
                         
     
-                                <div class="form-group row ">            
 
                                         <i class="fa fa-lock" id="icon"></i>
                                             <input id="typesPrompt" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password"  placeholder="الرقم السري " required>
@@ -76,23 +75,19 @@
                                                     <strong>{{ $errors->first('password') }}</strong>
                                                 </span>
                                             @endif
-                                    </div>
             
-                                    <div class="form-group row ">
                                             <i class="fa fa-lock" id="icon" ></i>
                                             <input id="typesPrompt" type="password" class="form-control" name="password_confirmation" placeholder="تأكيد الرقم السري  " required>
+                                  
                                             <span id="typePrompt">يرجى إعادة أدخال كلمة المرور </span>
 
-                                    </div>
-    
-
-                            
-
-                        </div>
-                     
-                        <input type="submit" class="btnRegister"  value="إنشاء حساب" />
-                        </form>
                     </div>
+                    <input type="submit" id="btnRegister" class="btnRegister"  value="إنشاء حساب" />
+
+                </div>   
+                    </form>
+                    </div>
+                    
                 </div>
                 
             </div>
